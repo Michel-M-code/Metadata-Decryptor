7e626909c76745b0c69e7253d166d2f07cfe5b41
Michel_M <96681374+Michel-M-code@users.noreply.github.com>
2025-02-24 16:00:59 +0300

    Fixed the version issue

--- MetadataDecryptor.py
+++ MetadataDecryptor.py
@@ -38,7 +38,7 @@ elif args.a and args.m:
 # Select mode
 while auto is None:
     user_input = input(f"{Fore.CYAN}Select mode. Automatic or Manual: {Style.RESET_ALL}").strip().lower()
-    
+
     if user_input[0] == 'a':
         auto = True
         print(f"{Fore.CYAN}Automatic mode selected.")
@@ -52,11 +52,14 @@ metadata_path = args.metadata
 reference_metadata_path = args.reference
 decrypted_metadata_path = args.output
 confirmed = False
+
+
 def reset_paths():
     global metadata_path, reference_metadata_path, decrypted_metadata_path
     metadata_path = args.metadata
     reference_metadata_path = args.reference
     decrypted_metadata_path = args.output
+
 
 while not confirmed or (not metadata_path and not reference_metadata_path and not decrypted_metadata_path):
     reset_paths()
@@ -64,9 +67,11 @@ while not confirmed or (not metadata_path and not reference_metadata_path and no
     if not metadata_path and not confirmed:
         metadata_path = input(f"{Fore.CYAN}Input encrypted metadata file path: {Style.RESET_ALL}").replace("\"", "")
     if not reference_metadata_path and not confirmed:
-        reference_metadata_path = input(f"{Fore.CYAN}Input reference metadata file path: {Style.RESET_ALL}").replace("\"", "")
+        reference_metadata_path = input(f"{Fore.CYAN}Input reference metadata file path: {Style.RESET_ALL}").replace(
+            "\"", "")
     if not decrypted_metadata_path and not confirmed:
-        decrypted_metadata_path = input(f"{Fore.CYAN}Input decrypted metadata save path: {Style.RESET_ALL}").replace("\"", "")
+        decrypted_metadata_path = input(f"{Fore.CYAN}Input decrypted metadata save path: {Style.RESET_ALL}").replace(
+            "\"", "")
 
     # Check if encrypted metadata is valid
     if not os.path.isfile(metadata_path):
@@ -75,7 +80,7 @@ while not confirmed or (not metadata_path and not reference_metadata_path and no
     elif open(metadata_path, "rb").read(4) != b'\xaf\x1b\xb1\xfa':
         print(f"{Fore.YELLOW}Encrypted metadata file is not a valid metadata file.")
         continue
-    
+
     # Check if reference metadata is valid
     if not os.path.isfile(reference_metadata_path):
         print(f"{Fore.YELLOW}Reference metadata file doesn't exist")
@@ -86,7 +91,8 @@ while not confirmed or (not metadata_path and not reference_metadata_path and no
 
     print(f"{Fore.CYAN}Using next files:")
     print(f"    {Fore.CYAN}Encrypted - {Fore.LIGHTMAGENTA_EX + Style.BRIGHT + metadata_path + Style.RESET_ALL}")
-    print(f"    {Fore.CYAN}Reference - {Fore.LIGHTMAGENTA_EX + Style.BRIGHT + reference_metadata_path + Style.RESET_ALL}")
+    print(
+        f"    {Fore.CYAN}Reference - {Fore.LIGHTMAGENTA_EX + Style.BRIGHT + reference_metadata_path + Style.RESET_ALL}")
     print(f"    {Fore.CYAN}Output - {Fore.LIGHTMAGENTA_EX + Style.BRIGHT + decrypted_metadata_path + Style.RESET_ALL}")
 
     while not confirmed:
@@ -96,15 +102,19 @@ while not confirmed or (not metadata_path and not reference_metadata_path and no
         except ValueError:
             print(f"{Fore.YELLOW}Enter 0 or 1")
 
-
 # Open files
 metadata = open(metadata_path, "rb")
 reference_metadata = open(reference_metadata_path, "rb")
 decrypted_metadata = open(decrypted_metadata_path, "wb")
 
-
-# Copy first 12 bytes.
-decrypted_metadata.write(reference_metadata.read(12))
+# Copy the magic.
+decrypted_metadata.write(reference_metadata.read(4))
+# Copy the version.
+metadata.seek(4)
+decrypted_metadata.write(metadata.read(4))
+# Copy the StringLiteralOffset.
+reference_metadata.seek(8)
+decrypted_metadata.write(reference_metadata.read(4))
 # In format (actual, reference): (difference, zero_difference_count)
 pairs_to_differences = {}
 # Skip 4 bytes
@@ -134,12 +144,13 @@ def compare_bytes_at_offsets(actual_offset, reference_offset, length) -> tuple:
 
     return difference, zero_difference_count
 
-bar = tqdm(total = 62 * 31, ncols = 100, colour = "green", unit = "pairs", desc="Searching... ")
+
+bar = tqdm(total=62 * 31, ncols=100, colour="green", unit="pairs", desc="Searching... ")
 
 # Parse every possible pair and check the difference.
 for actual_position in range(62):
     for reference_position in range(31):
-        
+
         bar.update()
 
         # Go to positions
@@ -157,8 +168,9 @@ for actual_position in range(62):
                 or candidate_actual == b"\x00\x01\x00\x00" or candidate_reference == b"\x00\x01\x00\x00"
                 or candidate_actual_int <= lowest_reference_offset - 1024):
             continue
-        
-        pairs_to_differences[(candidate_actual_int, candidate_reference_int)] = compare_bytes_at_offsets(candidate_actual_int, candidate_reference_int, 1024)
+
+        pairs_to_differences[(candidate_actual_int, candidate_reference_int)] = compare_bytes_at_offsets(
+            candidate_actual_int, candidate_reference_int, 1024)
 
 bar.close()
 
@@ -168,8 +180,8 @@ last_actual_value_found = 256
 last_reference_value_found = 256
 wrong_count = 0
 
-iterable = sorted(pairs_to_differences.items(),key=lambda x: (x[0][1] - x[1][1]), reverse=False)
-iterable_tqdm_wrapper = tqdm(iterable, ncols = 100, colour = "green", unit = "pairs", desc=f"Reordering...")
+iterable = sorted(pairs_to_differences.items(), key=lambda x: (x[0][1] - x[1][1]), reverse=False)
+iterable_tqdm_wrapper = tqdm(iterable, ncols=100, colour="green", unit="pairs", desc=f"Reordering...")
 
 # Write the decrypted metadata
 for (actual, reference), (difference, zero_difference) in iterable_tqdm_wrapper:
@@ -198,7 +210,8 @@ for (actual, reference), (difference, zero_difference) in iterable_tqdm_wrapper:
                 continue
 
         else:
-            print(f"Actual: {actual:,} Reference: {reference:,} | Size: {size:,} | Difference: {difference} Zero Difference: {zero_difference}")
+            print(
+                f"Actual: {actual:,} Reference: {reference:,} | Size: {size:,} | Difference: {difference} Zero Difference: {zero_difference}")
 
             # Ask the user if the value is valid.
             if bool(int(input("Valid? "))):
@@ -218,11 +231,11 @@ for (actual, reference), (difference, zero_difference) in iterable_tqdm_wrapper:
 if wrong_count > 30:
     print(f"{Fore.RED}DECRYPTION FAILED!")
     print(f"{Fore.YELLOW}Make sure you provided the right files, and if the decryption still fails, "
-            "feel free to open an issue on my github: https://github.com/Michel-M-code/Metadata-Decryptor/issues/new")
+          "feel free to open an issue on my github: https://github.com/Michel-M-code/Metadata-Decryptor/issues/new")
     exit(-1)
 
 # Write sizes
-for i in tqdm(range(len(sizes)), ncols = 100, colour = "green", unit = "sizes", desc=f"Writing sizes..."):
+for i in tqdm(range(len(sizes)), ncols=100, colour="green", unit="sizes", desc=f"Writing sizes..."):
     decrypted_metadata.seek(12 + i * 8)
     decrypted_metadata.write(struct.pack("<I", sizes[i]))
 
